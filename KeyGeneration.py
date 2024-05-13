ecc7dc9cee084673db4be33dce7a935173245f92
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2024-05-13 07:54:44 -0300

    Mensagens de erro na tela

--- KeyGeneration.py
+++ KeyGeneration.py
@@ -7,8 +7,8 @@ import ImportExportKey as iekey
 def rsa_keypair_generation(bits_key_length, privatekey_file_password, publickey_filename, privatekey_filename):
     
     key = RSA.generate(bits_key_length)
-    privatekey = iekey.export_RSA_privatekey("private/"+privatekey_filename, key, privatekey_file_password)
+    privatekey = iekey.export_RSA_privatekey("private/"+privatekey_filename + '.pem', key, privatekey_file_password)
     publickey = key.publickey()
-    iekey.export_RSA_publickey("public/"+publickey_filename, publickey)
+    iekey.export_RSA_publickey("public/"+publickey_filename + '.pem', publickey)
     
     return (publickey, privatekey)
